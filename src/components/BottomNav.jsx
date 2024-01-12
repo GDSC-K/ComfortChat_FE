45bb20a4e7d6b9f3189c67fc8f0e66d173ea682d
HarrySeop <parkpjs3083@naver.com>
2024-01-13 01:55:25 +0900

    Modify: Edit MenuBar

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './BottomNav.css';
-import { Home } from '../assets/icons/icon-home.png';
-import { CheckedHome } from '../assets/icons/icon-home-fill.png';
-import { History } from '../assets/icons/icon-book.png';
-import { CheckedHistory } from '../assets/icons/icon-book-fill.png';
-import { MyPage } from '../assets/icons/icon-person.png';
-import { CheckedMyPage } from '../assets/icons/icon-person-fill.png';
+import Home from '../assets/icons/icon-home.png';
+import CheckedHome from '../assets/icons/icon-home-fill.png';
+import History from '../assets/icons/icon-book.png';
+import CheckedHistory from '../assets/icons/icon-book-fill.png';
+import MyPage from '../assets/icons/icon-person.png';
+import CheckedMyPage from '../assets/icons/icon-person-fill.png';
 
 const BottomNav = () => {
   return (
