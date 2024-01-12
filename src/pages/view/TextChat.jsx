379b3437014b27c9a4477e1767cd39b70c52bb38
SEYEON-PARK <fourleaf123@naver.com>
2024-01-13 01:53:43 +0900

    Add TextView

--- src/pages/view/TextChat.jsx
+++ src/pages/view/TextChat.jsx
@@ -1,17 +1,21 @@
 import '../css/TextChat.css';
 import backIcon from '../../assets/icons/icon-back.png';
+import enterIcon from '../../assets/icons/icon-enter.png';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function TextChat() {
   return (
     <div className="App">
-      <div class="container">
-        <img src={backIcon} />
-        <div class="textBox">
-          <p class="text">텍스트로 물어보기</p>
-        </div>
-        <div class="voiceBox">
-          <p class="voice">음성으로 물어보기</p>
-        </div>
+      <div className="container">
+        <Link to="/" style={{ textDecoration: 'none' }}>
+          <img id="backIcon" src={backIcon} />
+        </Link>
+        <p id="twoIcon">
+          <input type="text" placeholder="궁금한 점을 물어보세요!" />
+          <div className="enter">
+            <img src={enterIcon} className="enterIcon" />
+          </div>
+        </p>
       </div>
     </div>
   );
